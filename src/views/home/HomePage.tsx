b35f1eb1d2db3cbb563f5ce14841c164d22ed2a9
culi <culitif@tuta.io>
2022-01-31 18:50:57 -0800

    create homepage

---
+++ src/views/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {};
+
+const HomePage: React.FC<Props> = () => {
+  return (
+    <div>
+      This is the home page
+    </div>
+  );
+};
+
+export default HomePage;
