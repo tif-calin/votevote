d8bc65e8aea567c76da2363d1aebf1fb6ab7146c
culi <culitif@tuta.io>
2022-02-11 03:04:38 -0800

    y-axis ticks!

--- src/components/charts/SimplePositiveBarChart.tsx
+++ src/components/charts/SimplePositiveBarChart.tsx
@@ -6,8 +6,11 @@ import useChartDimensions from '../../hooks/useChartDimensions';
 const Container = styled.div`
   height: 100%;
   width: 100%;
+  padding: 1rem;
   padding-bottom: 2rem;
   padding-left: 2rem;
+
+  font-weight: 100;
 
   & > svg {
     width: 100%;
@@ -19,6 +22,10 @@ const Container = styled.div`
 
   & rect {
     /* rx: 1.5; ry: 1.5; */
+  }
+
+  .y-axis {
+    font-size: 0.8rem;
   }
 `;
 
@@ -39,9 +46,12 @@ const SimplePositiveBarChart: React.FC<Props> = ({ data, barStyles }) => {
   ;
   
   const yScale = d3.scaleLinear()
-    .domain([0,Math.round(maxScore * 1.5)])
+    .domain([0,Math.round(maxScore * 1.25)])
     .range([height, 0])
   ;
+
+  const yTicks = yScale.nice().ticks();
+  console.log(yTicks);
 
   return (
     <Container ref={ref}>
@@ -67,9 +77,9 @@ const SimplePositiveBarChart: React.FC<Props> = ({ data, barStyles }) => {
             return (
               <g 
                 className="x-tick" 
-                transform={`translate(${xOffset}, 0)`}
+                transform={`translate(${xOffset},0)`}
               >
-                <line y2={10} strokeDasharray="2 2" />
+                <line y2={8} strokeDasharray="2 2" />
                 <text 
                   y={20}
                   textAnchor="middle"
@@ -83,6 +93,17 @@ const SimplePositiveBarChart: React.FC<Props> = ({ data, barStyles }) => {
         </g>
         <g className="y-axis">
           <line x1={0} y1={0} x2={0} y2={height} />
+          {yTicks.map((tick: number) => {
+            return (
+              <g className="y-tick" transform={`translate(0,${yScale(tick)})`}>
+                <line x2={-4} />
+                <text
+                  x={-8} y="0.25rem"
+                  textAnchor="end"
+                >{tick}</text>
+              </g>
+            );
+          })}
         </g>
       </svg>
     </Container>
