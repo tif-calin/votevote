d0a2127f234277b83d7e8ea016f79cff30dbec92
culi <culitif@tuta.io>
2022-02-28 02:03:06 -0800

    hehe awk

--- src/views/changelog/ChangelogPage.tsx
+++ src/views/changelog/ChangelogPage.tsx
@@ -81,7 +81,7 @@ const ChangelogPage: React.FC<Props> = () => {
           For more up to date information, please check out the changelog in the <A href="https://github.com/tif-calin/votevote/blob/main/CHANGELOG.md">GitHub repo</A>.
         </p>
 
-        <h3>v0.1.2 <span>2022_02feb2?</span></h3>
+        <h3>v0.1.2 <span>2022_02feb28</span></h3>
         <div className="release">
           <h4>Added</h4>
           <ul>
